e0ecfe1818bafbea5e07c5925d16c9b5336a0df0
Viktor Kojouharov <developer@sugr.org>
2014-07-29 23:10:59 +0300

    add a sitemap test

---
+++ middleware/sitemap_test.go
@@ -0,0 +1,106 @@
+package middleware
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/urandom/webfw"
+	"github.com/urandom/webfw/context"
+)
+
+func TestSitemapHandler(t *testing.T) {
+	c := context.NewContext()
+	l := log.New(os.Stderr, "", 0)
+	mw := Sitemap{
+		Pattern:          "/",
+		Prefix:           "http://example.com/",
+		RelativeLocation: "sitemap.xml",
+		Controllers:      []webfw.SitemapController{},
+	}
+
+	h := mw.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("test"))
+	}), c, l)
+
+	r, _ := http.NewRequest("GET", "http://example.com/en.all.json", nil)
+	rec := httptest.NewRecorder()
+
+	h.ServeHTTP(rec, r)
+
+	expected := []byte("test")
+	if !bytes.Equal(rec.Body.Bytes(), expected) {
+		t.Fatalf("Expected '%s', got '%s'\n", expected, rec.Body.Bytes())
+	}
+
+	r, _ = http.NewRequest("GET", "http://example.com/sitemap.xml", nil)
+	rec = httptest.NewRecorder()
+
+	h.ServeHTTP(rec, r)
+
+	expected = []byte(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+	
+</urlset>
+`)
+
+	if !bytes.Equal(rec.Body.Bytes(), expected) {
+		t.Fatalf("Expected '%s', got '%s'\n", expected, rec.Body.Bytes())
+	}
+
+	mw = Sitemap{
+		Pattern:          "/",
+		Prefix:           "http://example.com/",
+		RelativeLocation: "sitemap2.xml",
+		Controllers: []webfw.SitemapController{
+			sc{[]webfw.SitemapItem{webfw.SitemapItem{
+				Loc:        "/foo",
+				LastMod:    webfw.SitemapNoLastMod,
+				ChangeFreq: webfw.SitemapFrequencyDaily,
+				Priority:   0.5,
+			}}},
+		},
+	}
+
+	h = mw.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("test"))
+	}), c, l)
+
+	r, _ = http.NewRequest("GET", "http://example.com/sitemap2.xml", nil)
+	rec = httptest.NewRecorder()
+
+	h.ServeHTTP(rec, r)
+
+	expected = []byte(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+	
+		<url>
+			<loc>http://example.com/foo</loc>
+			
+			
+			<changefreq>daily </changefreq>
+			
+			
+			<priority>0.5</priority>
+			
+		</url>
+   
+</urlset>
+`)
+
+	if !bytes.Equal(rec.Body.Bytes(), expected) {
+		t.Fatalf("Expected '%s', got '%s'\n", expected, rec.Body.Bytes())
+	}
+
+}
+
+type sc struct {
+	items []webfw.SitemapItem
+}
+
+func (c sc) Sitemap(cont context.Context) []webfw.SitemapItem {
+	return c.items
+}
